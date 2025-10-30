e3024516ebb9070434b74353ed5dd593ca072692
swadha112 <swadha.khatod07@gmail.com>
2025-10-30 21:12:50 +0530

    fixed port

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const HOST = "0.0.0.0"; 
 
 app.get("/", (req, res) => {
   res.json({ message: "CI/CD with GitHub Actions is working!" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server running on http://${HOST}:${PORT}`);
 });
